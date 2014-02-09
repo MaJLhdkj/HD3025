6ea394e849ecb0f554e80d04a46d41aecc1c0f19
MaJLhdkj <yunxiangtju@163.com>
2014-02-09 15:00:46 +0800

    重新建立的VS2012工程，修改窗口标题为"HD3025上位机控制软件"

---
+++ UpperCtrl/UpperCtrl/UpperCtrlDlg.h
@@ -0,0 +1,32 @@
+
+// UpperCtrlDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CUpperCtrlDlg 对话框
+class CUpperCtrlDlg : public CDialog
+{
+// 构造
+public:
+	CUpperCtrlDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_UPPERCTRL_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
